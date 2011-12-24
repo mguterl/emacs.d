f9745f9c46ea69579e74f304c09c1c8fb4bbd8e5
Michael Guterl <michael@diminishing.org>
2011-12-24 13:09:06 -0500

    Use 16pt Inconsolata

--- custom.el
+++ custom.el
@@ -9,4 +9,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:stipple nil :background "#fdf6e3" :foreground "#002b36" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "apple" :family "Inconsolata")))))
