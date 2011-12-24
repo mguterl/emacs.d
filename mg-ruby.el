8bd5f62f5ce71552510dea527511bdbe0b228c7a
Michael Guterl <michael@diminishing.org>
2011-12-24 12:19:00 -0500

    Custom key bindings for ruby-mode

--- mg-ruby.el
+++ mg-ruby.el
@@ -1,3 +1,10 @@
+(eval-after-load 'ruby-mode
+  '(progn
+     (add-hook 'ruby-mode-hook 'inf-ruby-keys)
+     (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
+     (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)
+     (define-key ruby-mode-map (kbd "C-c l") "lambda")))
+
 ;; Rake files are ruby, too, as are gemspecs, rackup files, etc.
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
