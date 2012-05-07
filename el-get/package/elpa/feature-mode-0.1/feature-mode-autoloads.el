ca7c1061fe452050ce9d34fb310e47a8a21ddc8a
Michael Guterl <michael@diminishing.org>
2012-05-07 14:03:06 -0400

    Remove el-get/ and elpa/ from git tracking

--- el-get/package/elpa/feature-mode-0.1/feature-mode-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; feature-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (feature-mode) "feature-mode" "feature-mode.el"
-;;;;;;  (20222 5029))
-;;; Generated autoloads from feature-mode.el
-
-(autoload 'feature-mode "feature-mode" "\
-Major mode for editing plain text stories
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("feature-mode-pkg.el") (20222 5029 149147))
-
-;;;***
-
-(provide 'feature-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; feature-mode-autoloads.el ends here
