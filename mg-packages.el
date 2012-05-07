1bf3382179d77d13fc655c0949a0f01926d8da70
Michael Guterl <michael@diminishing.org>
2012-05-07 13:56:21 -0400

    Auto-install el-get

--- mg-packages.el
+++ mg-packages.el
@@ -3,7 +3,7 @@
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get") (unless (require 'el-get nil t) (url-retrieve "https://raw.github.com/dimitri/el-get/master/el-get-install.el" (lambda (s) (goto-char (point-max)) (eval-print-last-sexp))))
 (require 'el-get)
 
 (setq el-get-sources
