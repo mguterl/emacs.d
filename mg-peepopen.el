c6e2a409fde1a3191d967a79086e85f84a0ba93b
Michael Guterl <michael@diminishing.org>
2013-05-05 14:43:03 -0400

    Don't popup new frames for files opened with PeepOpen

--- mg-peepopen.el
+++ mg-peepopen.el
@@ -4,4 +4,7 @@
 ;; Not sure why this is required, something is funked up with peepopen.el
 (peepopen-bind-carbon-keys)
 
+;; Don't pop up files in a new frame
+(setq ns-pop-up-frames nil)
+
 (provide 'mg-peepopen)
