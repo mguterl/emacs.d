947027f82fd05a6cc4dd1b20c0dee7f839115c28
Michael Guterl <michael@diminishing.org>
2013-05-05 14:43:19 -0400

    Add function for formatting JSON

--- mg-javascript.el
+++ mg-javascript.el
@@ -1,3 +1,10 @@
 (setq js-indent-level 2)
 
+(defun json-format ()
+  (interactive)
+  (save-excursion
+    (shell-command-on-region (mark) (point) "python -c 'import sys,json; data=json.loads(sys.stdin.read()); print json.dumps(data,sort_keys=True,indent=2).decode(\"unicode_escape\").encode(\"utf8\",\"replace\")'" (buffer-name) t)
+    )
+  )
+
 (provide 'mg-javascript)
