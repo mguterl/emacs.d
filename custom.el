2a45a87ec57f0ab3f87819fb157f4337e3eda94e
Michael Guterl <michael@diminishing.org>
2013-09-17 18:14:12 -0400

    Add more safe themes

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("485737acc3bedc0318a567f1c0f5e7ed2dfde3fb" "e254937cba0f82c2d9eb3189a60748df9e486522" "1f392dc4316da3e648c6dc0f4aad1a87d4be556c" "1440d751f5ef51f9245f8910113daee99848e2c0" "517aecb1202bfa31fd3c44473d72483c5166124d" default))))
+ '(custom-safe-themes (quote ("71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" "d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" "485737acc3bedc0318a567f1c0f5e7ed2dfde3fb" "e254937cba0f82c2d9eb3189a60748df9e486522" "1f392dc4316da3e648c6dc0f4aad1a87d4be556c" "1440d751f5ef51f9245f8910113daee99848e2c0" "517aecb1202bfa31fd3c44473d72483c5166124d" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
